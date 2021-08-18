f03f5297d597b2dff3fe403af821380309fe8a51
clayderhua@gmail.com <advrisc@gmail.com>
2021-08-18 11:34:25 +0800

    u-boot initial commit tag LSDK-20.12

---
+++ board/freescale/common/qsfp_eeprom.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2020 NXP
+ * Copyright 2004 Freescale Semiconductor.
+ */
+
+#ifndef __QSFP_EEPROM_H_
+#define __QSFP_EEPROM_H_
+/*
+ * QSFP eeprom reader external API interface.
+ */
+
+/* return the ethernet compatibility field 0 */
+unsigned char get_qsfp_compat0(void);
+#endif  /* __QSFP_EEPROM_H_ */
